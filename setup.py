dc981a496b0592aa1a9171fba72c3f5356284e1b
Camron Godbout <camron248@gmail.com>
2017-10-03 20:56:19 -0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='surveyor',
+      version='0.1',
+      description='EDA and statistical analysis package',
+      url='http://github.com/camrongodbout/surveyor',
+      author='Camron Godbout',
+      author_email='camron248@gmail.com',
+      license='MIT',
+      packages=['surveyor'],
+      zip_safe=False)
